fd40e4385292d59d6aeaff4f8fe5f949aaf69cfe
Eliene Francelino <elienefrancelino@outlook.com>
2025-01-15 15:05:07 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,4 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+//TODO: Conhecer e importar a classe Scanner
+//Exibir as mensagens para o nossso usuário
+//Obeter pela scanner os valores digitados no terminal
+//Exibir a mensagem conta criada
